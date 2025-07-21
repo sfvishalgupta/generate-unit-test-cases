3308618472ccb8cbd6533cdc551a7d36946d34fa
Vishal Gupta <vishal.gupta2@Vishal-SFIN185-2.local>
2025-07-21 16:59:16 +0530

    update

--- environment.ts
+++ environment.ts
@@ -1,27 +1,28 @@
-import fs from 'fs';
-import { config } from 'dotenv';
-import { logger } from 'OpenRouterAICore/pino';
-import { CustomError } from 'OpenRouterAICore/customError';
-import { ERRORS } from 'OpenRouterAICore/environment';
+import fs from "fs";
+import { config } from "dotenv";
+import { logger } from "OpenRouterAICore/pino";
+import { CustomError } from "OpenRouterAICore/customError";
+import { ERRORS } from "OpenRouterAICore/environment";
 
-
-if (fs.existsSync('.env')) {
-    logger.info('Loading env file from .env');
-    config({ path: '.env' });
+if (fs.existsSync(".env")) {
+  logger.info("Loading env file from .env");
+  config({ path: ".env" });
 }
-if (process.env.NODE_ENV != '' && fs.existsSync(`.env.${process.env.NODE_ENV}`)) {
-    logger.info(`Loading env file from .env.${process.env.NODE_ENV}`);
-    config({ path: `.env.${process.env.NODE_ENV}` });
+if (
+  process.env.NODE_ENV != "" &&
+  fs.existsSync(`.env.${process.env.NODE_ENV}`)
+) {
+  logger.info(`Loading env file from .env.${process.env.NODE_ENV}`);
+  config({ path: `.env.${process.env.NODE_ENV}` });
 } else {
-    logger.info(`stage not set.`);
+  logger.info(`stage not set.`);
 }
 
 export const ENV_VARIABLES = {
-    LOCAL_MACHINE: process.env.LOCAL_MACHINE ?? '0',
-    // OUTPUT_REPO: process.env.OUTPUT_REPO?.trim() ?? ''
-}
+  LOCAL_MACHINE: process.env.LOCAL_MACHINE ?? "0",
+  // OUTPUT_REPO: process.env.OUTPUT_REPO?.trim() ?? ''
+};
 
 // if(ENV_VARIABLES.OUTPUT_REPO === ''){
 //     throw new CustomError(ERRORS.ENV_NOT_SET, 'OUTPUT_REPO not set.');
 // }
-
--- main.ts
+++ main.ts
@@ -1,119 +1,139 @@
-import fs from 'fs';
+import fs from "fs";
 import {
-    GetProjectDocument,
-    GetJiraTitle,
-    GetUserPrompt,
-} from './OpenRouterAICore/thirdPartyUtils';
-import { GetStore } from './OpenRouterAICore/store/utils';
-import { ENV_VARIABLES as GlobalENV } from './OpenRouterAICore/environment';
-import { CustomError } from './OpenRouterAICore/customError';
-import { logger } from 'OpenRouterAICore/pino';
-import { ENV_VARIABLES } from './environment';
-import * as path from 'path';
+  GetProjectDocument,
+  GetJiraTitle,
+  GetUserPrompt,
+} from "./OpenRouterAICore/thirdPartyUtils";
+import { GetStore } from "./OpenRouterAICore/store/utils";
+import { ENV_VARIABLES as GlobalENV } from "./OpenRouterAICore/environment";
+import { CustomError } from "./OpenRouterAICore/customError";
+import { logger } from "OpenRouterAICore/pino";
+import { ENV_VARIABLES } from "./environment";
+import * as path from "path";
 
 const output: string = "./output/" + Date.now();
 const createOutputFilesBackend = (outFolder: string, storeResponse: string) => {
-    const outputPath: string = outFolder + "api/";
-    logger.info(`Creating output files for Backend:- ${outputPath}`);
-    const regex: RegExp = /```typescript([\s\S]*?)```/g;
-    const match: RegExpMatchArray | null = storeResponse.match(regex);
-    const regexFile: RegExp = /---filePath:([\s\S]*?)---/g;
-    if (match) {
-        for (const aMatch of match) {
-            try {
-                const fileContent: string = aMatch.replace(/```typescript|```/g, '').trim();
-                const m1 = fileContent.match(regexFile);
-                if (m1) {
-                    for (const m of m1) {
-                        const fileName: string = m.replace(/---filePath:|---/g, '').trim();
-                        fs.mkdirSync(outputPath + path.dirname(fileName), { recursive: true });
-                        fs.writeFileSync(
-                            outputPath + fileName,
-                            fileContent.replace(/---filePath:.*?---/g, '').trim()
-                        );
-                    }
-                }
-            } catch (e) {
-                console.log(e);
-            }
+  const outputPath: string = outFolder + "api/";
+  logger.info(`Creating output files for Backend:- ${outputPath}`);
+  const regex: RegExp = /```typescript([\s\S]*?)```/g;
+  const match: RegExpMatchArray | null = storeResponse.match(regex);
+  const regexFile: RegExp = /---filePath:([\s\S]*?)---/g;
+  if (match) {
+    for (const aMatch of match) {
+      try {
+        const fileContent: string = aMatch
+          .replace(/```typescript|```/g, "")
+          .trim();
+        const m1 = fileContent.match(regexFile);
+        if (m1) {
+          for (const m of m1) {
+            const fileName: string = m.replace(/---filePath:|---/g, "").trim();
+            fs.mkdirSync(outputPath + path.dirname(fileName), {
+              recursive: true,
+            });
+            fs.writeFileSync(
+              outputPath + fileName,
+              fileContent
+                .split(`---filePath:${fileName}---`)[1]
+                .split("---filePath")[0]
+                .trim(),
+              // fileContent.replace(/---filePath:.*?---/g, '').trim()
+            );
+          }
         }
+      } catch (e) {
+        console.log(e);
+      }
     }
-}
+  }
+};
 
-const createOutputFilesFrontend = (outFolder: string, storeResponse: string) => {
-    const outputPath: string = outFolder + "ui/";
-    const regex: RegExp = /```custom_code_for_frontend([\s\S]*?)```/g;
-    const match: RegExpMatchArray | null = storeResponse.match(regex);
-    const regexFile = /---filePath:([\s\S]*?)---/g;
-    if (match) {
-        for (const aMatch of match) {
-            try {
-                const fileContent: string = aMatch.replace(/```custom_code_for_frontend|```/g, '').trim();
-                const m1 = fileContent.match(regexFile);
-                if (m1) {
-                    const fileName: string = m1[0].replace(/---filePath:|---/g, '').trim();
-                    fs.mkdirSync(outputPath + path.dirname(fileName), { recursive: true });
-                    fs.writeFileSync(
-                        outputPath + fileName,
-                        fileContent.replace(/---filePath:.*?---/g, '').trim()
-                    );
-                }
-            } catch (e) {
-                console.log(e);
-            }
+const createOutputFilesFrontend = (
+  outFolder: string,
+  storeResponse: string,
+) => {
+  const outputPath: string = outFolder + "ui/";
+  const regex: RegExp = /```custom_code_for_frontend([\s\S]*?)```/g;
+  const match: RegExpMatchArray | null = storeResponse.match(regex);
+  const regexFile = /---filePath:([\s\S]*?)---/g;
+  if (match) {
+    for (const aMatch of match) {
+      try {
+        const fileContent: string = aMatch
+          .replace(/```custom_code_for_frontend|```/g, "")
+          .trim();
+        const m1 = fileContent.match(regexFile);
+        if (m1) {
+          const fileName: string = m1[0]
+            .replace(/---filePath:|---/g, "")
+            .trim();
+          fs.mkdirSync(outputPath + path.dirname(fileName), {
+            recursive: true,
+          });
+          fs.writeFileSync(
+            outputPath + fileName,
+            fileContent.replace(/---filePath:.*?---/g, "").trim(),
+          );
         }
+      } catch (e) {
+        console.log(e);
+      }
     }
-}
+  }
+};
 
 const logToFile = (path: string, content: string) => {
-    if (ENV_VARIABLES.LOCAL_MACHINE == "1") {
-        fs.writeFileSync(path, content);
-    }
-}
+  if (ENV_VARIABLES.LOCAL_MACHINE == "1") {
+    fs.writeFileSync(path, content);
+  }
+};
 const main = async (): Promise<string> => {
-    let response = "";
-    try {
-        const ticketDetails: string = await GetJiraTitle();
-        logToFile('./tmp/' + GlobalENV.JIRA_TICKET_ID + '.txt', ticketDetails);
+  let response = "";
+  try {
+    const ticketDetails: string = await GetJiraTitle();
+    logToFile("./tmp/" + GlobalENV.JIRA_TICKET_ID + ".txt", ticketDetails);
 
-        const projectDocument: string = await GetProjectDocument(
-            GlobalENV.PROJECT_DOCUMENT_PATH
-        );
-        logToFile('./tmp/projectDocument.txt', projectDocument);
+    const projectDocument: string = await GetProjectDocument(
+      GlobalENV.PROJECT_DOCUMENT_PATH,
+    );
+    logToFile("./tmp/projectDocument.txt", projectDocument);
 
-        const store = GetStore();
-        await store.addDocument(GlobalENV.JIRA_PROJECT_KEY + '-index', projectDocument);
+    const store = GetStore();
+    await store.addDocument(
+      GlobalENV.JIRA_PROJECT_KEY + "-index",
+      projectDocument,
+    );
 
-        let askPrompt: string = await GetUserPrompt();
-        askPrompt = askPrompt.replace("##JIRA_DETAILS##", ticketDetails);
-        logToFile('./tmp/prompt.txt', askPrompt);
+    let askPrompt: string = await GetUserPrompt();
+    askPrompt = askPrompt.replace("##JIRA_DETAILS##", ticketDetails);
+    logToFile("./tmp/prompt.txt", askPrompt);
 
-        const modelArr: string[] = GlobalENV.OPEN_ROUTER_MODEL.trim().split(",");
-        for (const model of modelArr) {
-            logger.info(`For Model ${model.trim()}`)
-            const storeResponse: string = await store.generate(
-                model.trim(),
-                GlobalENV.JIRA_PROJECT_KEY + '-index',
-                askPrompt.trim()
-            );
-            logToFile('./tmp/output.md', storeResponse);
-            const outputFolder = output + '-' + model.trim().replace("/", "-") + '/';
-            createOutputFilesBackend(outputFolder, storeResponse);
-            // createOutputFilesFrontend(output + '-' + model.trim() + '/', storeResponse);
-        }
-    } catch (error) {
-        if (error instanceof CustomError) {
-            response = `${error.toString()}`;
-        } else if (error instanceof Error) {
-            response = `❌ Action failed: ${error.message}`;
-        } else {
-            response = `❌ Action failed: ${String(error)}`;
-        }
-        logger.error(response);
-    } finally {
-        // Do not return from finally block
+    const modelArr: string[] = GlobalENV.OPEN_ROUTER_MODEL.trim().split(",");
+    for (const model of modelArr) {
+      logger.info(`For Model ${model.trim()}`);
+      const storeResponse: string = await store.generate(
+        model.trim(),
+        GlobalENV.JIRA_PROJECT_KEY + "-index",
+        askPrompt.trim(),
+      );
+      logToFile("./tmp/output.md", storeResponse);
+      const outputFolder = output + "-" + model.trim().replace("/", "-") + "/";
+      createOutputFilesBackend(outputFolder, storeResponse);
+      // createOutputFilesFrontend(output + '-' + model.trim() + '/', storeResponse);
     }
-    return response
-}
+  } catch (error) {
+    if (error instanceof CustomError) {
+      response = `${error.toString()}`;
+    } else if (error instanceof Error) {
+      response = `❌ Action failed: ${error.message}`;
+    } else {
+      response = `❌ Action failed: ${String(error)}`;
+    }
+    logger.error(response);
+  } finally {
+    // Do not return from finally block
+  }
+  return response;
+};
 
 main();
